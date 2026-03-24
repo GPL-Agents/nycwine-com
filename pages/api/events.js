b8cb055423577c7eb568e90e9d432854d03ddac1
Greg Loeffelholz <gloeff@yahoo.com>
2026-03-24 13:42:46 -0400

    Update events.js

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,23 +1,34 @@
 export default async function handler(req, res) {
   try {
-    const url = 'https://www.eventbrite.com/d/ny--new-york/wine/';
+    const searchUrl = 'https://www.eventbrite.com/d/ny--new-york/wine/';
 
-    const response = await fetch(url);
+    const response = await fetch(searchUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+      },
+    });
+
     const html = await response.text();
 
-    // VERY simple extraction: grab event titles from page text
-    const matches = [...html.matchAll(/"name":"([^"]+)"/g)];
+    const titleMatches = [...html.matchAll(/"name":"([^"]+)"/g)];
+    const linkMatches = [...html.matchAll(/"url":"(https:\\\/\\\/www\.eventbrite\.com\\\/e\\\/[^"]+)"/g)];
 
-    const events = matches
-      .slice(0, 20)
-      .map((m, i) => ({
+    const events = titleMatches.slice(0, 20).map((m, i) => {
+      let eventUrl = linkMatches[i]?.[1] || searchUrl;
+
+      eventUrl = eventUrl
+        .replace(/\\\//g, '/')
+        .replace(/\\\\u002[Ff]/g, '/');
+
+      return {
         id: i + 1,
         title: m[1],
         venue: 'New York City',
         date: null,
-        url: url,
+        url: eventUrl,
         source: 'Eventbrite',
-      }));
+      };
+    });
 
     return res.status(200).json(events);
   } catch (err) {
